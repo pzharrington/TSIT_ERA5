2b2b17cf9eac7c80f6f3af8471fdc42919c0b072
Peter Harrington <pharrington@lbl.gov>
2022-05-31 14:01:52 -0700

    actually add submit script

---
+++ submit.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+#SBATCH --time=06:00:00
+#SBATCH -C gpu
+#SBATCH --account=m4134_g
+#SBATCH -q regular
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --gpus-per-node=4
+#SBATCH --cpus-per-task=32
+#SBATCH --image=nersc/pytorch:ngc-22.02-v0
+
+ROOT_DIR=$SCRATCH/tsitprecip/era5demo
+export HDF5_USE_FILE_LOCKING=FALSE
+args="${@}"
+
+export NCCL_NET_GDR_LEVEL=PHB
+export MASTER_ADDR=$(hostname)
+
+set -x
+srun -u --mpi=pmi2 shifter --module gpu \
+    bash -c "
+    source export_DDP_vars.sh
+    python train.py --root_dir=${ROOT_DIR} ${args}
+    "
+
+#shifter --module gpu \
+#    bash -c "
+#    python train.py --root_dir=${ROOT_DIR} ${args}
+#    "
